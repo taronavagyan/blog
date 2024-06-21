bec89bd306affbca87fda02809acf5d4f304766d
Taron <88250538+taronavagyan@users.noreply.github.com>
2024-06-21 12:05:20 -0700

    chapter 7.1 completed

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root "articles#index"
 
   get "/articles", to: "articles#index"
+  get "/articles/:id", to: "articles#show"
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
