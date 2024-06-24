93ef919e223e0f05f1d0523b8bd79011a0dfe96d
Taron <88250538+taronavagyan@users.noreply.github.com>
2024-06-23 21:42:13 -0700

    chapter 8 completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
+
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
